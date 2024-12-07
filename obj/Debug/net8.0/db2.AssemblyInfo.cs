5337a50a1a3590b711135002447160dbdc5827a9
Abdelrahman Taha <86110947+Hacker1one@users.noreply.github.com>
2024-12-07 14:04:04 +0200

    First Commit

--- obj/Debug/net8.0/db2.AssemblyInfo.cs
+++ obj/Debug/net8.0/db2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("db2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d91d6b07b025cddfbcf4df059f7b1cb4c2402b03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7676695573260cd1aa39d4af529d9ccdef164cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("db2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("db2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
